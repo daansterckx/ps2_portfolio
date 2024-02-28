68427764b87b806d973e92a7747f9b816e45102f
dsterckx <39022656+daansterckx@users.noreply.github.com>
2024-02-28 11:57:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// JavaScript
+function openModal1() {
+    document.getElementById("myModal1").style.display = "block";
+}
+  function closeModal1() {
+    document.getElementById("myModal1").style.display = "none";
+}
+  function openModal2() {
+    document.getElementById("myModal2").style.display = "block";
+}
+  function closeModal2() {
+    document.getElementById("myModal2").style.display = "none";
+}
+  function openModal3() {
+    document.getElementById("myModal3").style.display = "block";
+} 
+  function closeModal3() {
+    document.getElementById("myModal3").style.display = "none";
+}
+  function openModal4() {
+    document.getElementById("myModal4").style.display = "block";
+}
+  function closeModal4() {
+    document.getElementById("myModal4").style.display = "none";
+}
